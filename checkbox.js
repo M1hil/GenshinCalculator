c026270aabb50af1c9b84b578fc469c2dd8d9f0e
M1hil <coolmich1234@gmail.com>
2023-01-12 14:04:49 +0100

    Date fixed/updated (Dailys funktionieren)

--- checkbox.js
+++ checkbox.js
@@ -1,21 +1,19 @@
 function checkAddress()
 {
-    var dailys1 = document.getElementById('dailys');
-    var codes1 = document.getElementById('codes');
-    var compensations1 = document.getElementById('compensations');
-    var events1 = document.getElementById('events');
-    var trials1 = document.getElementById('trials');
-    var abyss1 = document.getElementById('abyss');
-    var shopreset1 = document.getElementById('shopreset');
-    var battlepassfree1 = document.getElementById('battlepassfree');
-    var battlepasspaied1 = document.getElementById('battlepasspaied');
-    var welkin1 = document.getElementById('welkin');
+    var dailys1             = document.getElementById('dailys');
+    var codes1              = document.getElementById('codes');
+    var compensations1      = document.getElementById('compensations');
+    var events1             = document.getElementById('events');
+    var trials1             = document.getElementById('trials');
+    var abyss1              = document.getElementById('abyss');
+    var shopreset1          = document.getElementById('shopreset');
+    var battlepassfree1     = document.getElementById('battlepassfree');
+    var battlepasspaied1    = document.getElementById('battlepasspaied');
+    var welkin1             = document.getElementById('welkin');
     let primos = 0;
-    if (dailys1.checked)
-    {
-        primos+=60 * diffDays;
-
-        console.log("ja");
+    if (dailys1.checked){
+    var daysDiff = clcdate()
+        primos += 60 * daysDiff;
     }
     if (codes1.checked)
     {
