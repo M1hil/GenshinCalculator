8ed6790bb03d32c2f61e63532e94728b6bac177a
M1hil <coolmich1234@gmail.com>
2023-01-14 14:57:43 +0100

    form wieder hinzugefügt, fehler mit value dailys1

--- javascriptfiles.js
+++ javascriptfiles.js
@@ -1,20 +1,14 @@
-const datezero = new Date(2022, 11, 7); // 7.11.2022 ist Tag 0
-    document.getElementById("hal1").innerHTML = datezero;
+const datezero = new Date(2022, 11, 7);                         // 7.11.2022 ist Tag 0
 let milzero = datezero.getTime();
-let dayszero = Math.ceil(milzero / (1000 * 60 * 60 * 24));  // Tag 0 in Tagen von Anfang
-    document.getElementById("hal2").innerHTML = dayszero;
-const datetoday = new Date();   // aktueller Tag
-    document.getElementById("hal3").innerHTML = datetoday;
+let dayszero = Math.ceil(milzero / (1000 * 60 * 60 * 24));      // Tag 0 in Tagen von Anfang
+const datetoday = new Date();                                   // aktueller Tag
 let miltoday = datetoday.getTime();
-let daystoday = Math.floor(miltoday / (1000 * 60 * 60 * 24));   // aktueller Tag in Tagen von ANfang
-    document.getElementById("hal4").innerHTML = daystoday;
+let daystoday = Math.floor(miltoday / (1000 * 60 * 60 * 24));   // aktueller Tag in Tagen von Anfang
 
 function clcupdate() {
-    let dateDiff = daystoday - dayszero;    // Tage von Tag 0 bis heute
-        document.getElementById("hal5").innerHTML = dateDiff;
+    let dateDiff = daystoday - dayszero;                        // Tage von Tag 0 bis heute
     var daysDiff = clcdate()
-    daysleft = dateDiff % 21;   // Tage seit 3 Wochen Update
-        document.getElementById("hal6").innerHTML = daysleft;
+    daysleft = dateDiff % 21;                                   // Tage seit 3 Wochen Update
     if (daysDiff <=21){
         if (daysleft + daysDiff >= 21){
             let threeweeks = 1;
@@ -24,7 +18,7 @@ function clcupdate() {
             let threeweeks = 0;
             return threeweeks;
         }
-    }
+    } 
     else {
         timesover21days = Math.floor(daysDiff / 21);
         restover21days =  daysDiff % 21;
